422f44cf5fe29f8c89ee039aff403107e089aae6
Susan Tan <suetan93@gmail.com>
2020-08-12 15:46:28 -0700

    Header test added/refactored

--- client/src/components/TitleBar.jsx
+++ client/src/components/TitleBar.jsx
@@ -13,6 +13,7 @@ const Wrapper = styled.div`
   letter-spacing: normal;
 `;
 
+// use props in styled components for conditonal renderings
 const Grid = styled.div`
   display: grid;
   grid-template-columns: 15px 34px 45px 90px auto auto 77px 70px;
@@ -43,7 +44,7 @@ const TitleBar = (props) => (
       <Star>&#9733;</Star>
       <Nested><b>{props.listing.rating}</b></Nested>
       <Nested>({props.listing.totalratings}) ·</Nested>
-      <Nested>i Superhost ·</Nested>
+      {<Nested>i Superhost ·</Nested>}
       <Nested>{props.listing.location}</Nested>
       <Nested />
       <Nested>
--- client/src/tests/Header.test.js
+++ client/src/tests/Header.test.js
@@ -5,7 +5,13 @@ import { shallow } from 'enzyme';
 import Header from '../components/Header';
 
 describe('Header Component', () => {
-  it('Should render Header correctly', () => {
-    shallow(<Header />);
+  it('Should render Header component', () => {
+    const wrapper = shallow(<Header />);
+    expect(wrapper.exists()).toBe(true);
+  });
+
+  it('Should find a div element', () => {
+    const wrapper = shallow(<Header />);
+    expect(wrapper.find('div').exists()).toBe(true);
   });
 });
